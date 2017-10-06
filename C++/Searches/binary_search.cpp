fcdbc458527fa18601c60c49eea7e9f2b2399a3e
Shifali Gupta <shifali1998@gmail.com>
2017-10-06 15:26:06 +0530

    Updated Binary Search

--- C++/Searches/binary_search.cpp
+++ C++/Searches/binary_search.cpp
@@ -1,46 +1,55 @@
 #include<iostream>
 using namespace std;
-struct treeNode
-{
-    int data;
-    struct treeNode *left;
-    struct treeNode *right;
-};
-struct treeNode *root = NULL;
-struct treeNode *inserts(struct treeNode *root, int num)
-{
-    if(root == NULL)
-    {
-        struct treeNode *nnode = new struct treeNode;
-        nnode->data = num;
-        nnode->left = nnode->right = NULL;
-        root = nnode;
-        return root;
-    }
-    if(num > root->data)
-        root->right = inserts(root->right, num);
-    else
-        root->left = inserts(root->left, num);
-    return root;
-}
-void inorder(struct treeNode *root)
-{
-    if(root == NULL)
-        return;
-    inorder(root->left);
-    cout<<root->data<<" ";
-    inorder(root->right);
-}
 int main()
 {
-    int n, num;
-    cout<<"enter no of elements to insert: ";
+    int n,a[100],x,mid=0,s=0,e=0;
+    cout<<"Enter size of array: ";
     cin>>n;
-    cout<<"enter element to insert: ";
-    while(n){
-        cin>>num;
-        root = inserts(root, num);
-        n--;
+    cout<<"Enter elements of array: \n";
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
     }
-    inorder(root);
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n-i; j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+    cout<<"Sorted array: "<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cout<<a[i]<<" "<<endl;
+    }
+    e = n-1;
+    cout<<"Enter a no to search: ";
+    cin>>x;
+    mid = (s+e)/2;
+    while(s<e && a[mid]!=x)
+    {
+        if(x>a[mid])
+        {
+            s = mid+1;
+        }
+        else
+        {
+            e = mid-1;
+        }
+        mid = (s+e)/2;
+    }
+    if(a[mid] == x)
+    {
+        cout<<"Element "<<a[mid]<<" found at "<<mid+1<<" position"<<endl;
+    }
+    else
+    {
+        cout<<"Element not found"<<endl;
+    }
+    return 0;
 }
