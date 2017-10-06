42fc7be9401078e24d7ceb4ed3c5501655bf8ce6
Prince Pathria <princepathria@gmail.com>
2017-10-06 15:13:17 +0530

    Added linear search in python3 (updated)

--- Python/Searches/linear_search.py
+++ Python/Searches/linear_search.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
-# A random list
-l = [1, 5, 4, 2, 3]
-rand_no = 4
-# Linear Search to find a random no
-i = 0
-for var in l:
-    if var == rand_no:
-        print(i)
-        break;
-    i = i +1
+def main():
+    array = []
+    print("Enter numbers to add in array and -9999 to stop")
+    while True:
+        user_input = int(input())
+        if user_input == -9999:
+            break
+        array = array + [user_input]
+    print (array)
+    linear_search(array, int(input("Enter number to search\n")))
 
+def linear_search(array, num):
+    pos  = 0
+    for x in array:
+        if x == num:
+            print(pos)
+            return
+        pos = pos + 1
+    print("Element not found")
+
+if __name__ == '__main__':
+    main()
