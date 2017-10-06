70776474c44d7cfd218c6060b391f510906e792f
ankush4144 <ankushgoyal310@gmail.com>
2017-10-06 15:21:18 +0530

    program of binary search in c

---
+++ C/Searches/search binary.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+int main()
+{
+
+int c,first,last,middle,n,search,array[100];
+
+printf("enter the no. of elements\n");
+scanf("%d",&n);
+
+printf("enter %d integers\n",n);
+
+for(c = 0; c < n;c++)
+    scanf("%d",&array[c]);
+
+printf("enter value to find\n");
+scanf("%d",&search);
+
+first=0;
+last=n-1;
+middle=(first+last)/2;
+
+while(first<=last){
+if(array[middle]<search)
+    first=middle+1;
+    else if(array[middle] == search){
+    printf("%d found at location %d.\n",search,middle+1);
+    break;
+    }
+else if(array[middle] == search){
+printf("%d found at location %d.\n",search,middle+1);
+break;
+}
+else
+last=middle-1;
+
+middle=(first+last)/2;
+}
+
+if(first>last)
+printf("number not found! %d is not present in the list.\n",search);
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
