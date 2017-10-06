13f71a484f7c8303b063d905ca7296d3090f2ba4
DFG <cocaine31197@gmail.com>
2017-10-06 16:03:01 +0530

    Update

--- Python/Conversions/binaryToDecimal.py
+++ Python/Conversions/binaryToDecimal.py
@@ -1,8 +1,20 @@
-temp = raw_input("Please Enter a Binary number")
-ques = temp[::-1]
-count = 0
-res = 0
-flag = 0
-for x in ques:
-    res += int(x)*(2**count)
-    count += 1
+while True:
+    print("Please Enter a Binary number or Type EXIT to exit")
+    temp = raw_input()
+    if(temp != "EXIT"):
+        ques = temp[::-1]
+        count = 0
+        res = 0
+        flag = 0
+        for x in ques:
+            if(int(x)>1):
+                print("Invalid. Enter a binary number")
+                flag = 1
+                break
+            res += int(x)*(2**count)
+            count += 1
+        if not flag:
+            print res
+    else:
+        break
+print ("End of program")
