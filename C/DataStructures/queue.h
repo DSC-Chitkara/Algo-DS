58ef647e715a410f21858e4746df4cdb9fd36aae
bharat786 <bharat.786rawat@gmail.com>
2017-10-09 04:16:06 +0530

    Added Data structures algorithms in c

---
+++ C/DataStructures/queue.h
@@ -0,0 +1,21 @@
+// Header file.
+// This file contains all the declarations
+struct Q{
+	int A[10];
+	
+	int front;
+	int rear;
+};
+
+
+
+//typedef struct Q *Q;
+
+
+void Enqueue(int ,struct Q* );
+
+void dequeue(struct Q* );
+
+void print(struct Q* );
+
+//void create();
