fffcbaae2d8c28909d44a206afc79e689820d0fc
priyansh agrawal <priyanshagarwal02may@gmail.com>
2017-10-10 18:33:23 +0530

    updated pernicious.py
    modified the code with best complexity possible

--- Python/Misc/pernicious.py
+++ Python/Misc/pernicious.py
@@ -8,39 +8,41 @@ is a pernicious prime or not'''
 '''According to number theory , a positive number is pernicious prime if 
 sum of the digit of this number in binary form is prime'''
 
+#################### Overall complexity is O(sqrt(n)*log(n))  ############################
 
-def binary(num):#thisfunction coverts decimalnumber into its binary equivalent form 
-	base=1
-	binary=0
-	while num>0:
-		rem=num%2
-		binary=binary+rem*base
-		num=num/2
-		base=base*10
+import math
 
-	return binary
 
-def ones(binary): #this function would calculates sum of all set bits in binary number
+def isprime(num): #function to check whether a number is prime or not 
+	if num<3 or num==3:
+		return 1
+	if num%2==0 or num%3==0:
+		return 0
+	for i in range(5,int(math.sqrt(num))+1,6):
+		if num%i==0 or num%(i+2)==0:
+			return 0
+	return 1		########## O(sqrt(n))		
+
+def count_ones(num): #function to calculate the number of set bits in a number 
 	count=0
-	while binary!=0:
-		if binary%10==1:
-			count+=1
-		binary=binary/10
-	return count
+	while num:
+		num = (num&(num-1))
+		count+=1
 
-f=0
-num = input("Enter any positive integer: ") #other than 1 and 2 
-if num!=1 and num!=2:
+	return count	########## O(log(n))
 
-	d = binary(num)			
-	o = ones(d)
-	for i in range(2,o/2+1):
-		if 0%i==0:
-			f=1
-		break
-	if f!=1 and num!=1:
-		print "yes\n"
-	elif f==1:
-		print "no\n"
-else:
-	print "Please enter any number other tha 1 or 2\n"
+while 1:
+
+	num = input("Enter any positive integer: ") #other than 1 and 2 
+	if num!=1 and num!=2:
+
+		total_set_bits = count_ones(num) #since sum of set bits is equivalent to counting those bits 
+		print total_set_bits
+		flag=isprime(total_set_bits)
+		print flag
+		if flag==1:
+			print "yes\n"
+		else:
+			print "no\n"	
+	else:
+		print "Please enter any number other tha 1 or 2\n"
